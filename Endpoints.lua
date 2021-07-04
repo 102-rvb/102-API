4ff2e9bc2a47bd5da12882a11f7a8accd2f48bc1
diltz <38502930+Diltz@users.noreply.github.com>
2021-07-05 06:17:33 +0700

    Create Endpoints.lua

---
+++ Endpoints.lua
@@ -0,0 +1,36 @@
+return {
+	KickQueue = {
+		Path = "/kick/queue",
+		Method = "GET"
+	},
+	
+	KickRemove = {
+		Path = "/kick/remove",
+		Method = "DELETE"
+	},
+	
+	KickAdd = {
+		Path = "/kick/add",
+		Method = "POST"
+	},
+	
+	BanlistGetUserInfo = {
+		Path = "/banlist/getuserinfo",
+		Method = "GET"
+	},
+	
+	BanlistAdd = {
+		Path = "/banlist/add",
+		Method = "POST"
+	},
+	
+	BanlistRemove = {
+		Path = "/banlist/remove",
+		Method = "DELETE"
+	},
+	
+	GetUsersBannedData = {
+		Path = "/banlist/getstatuslist",
+		Method = "POST"
+	}
+}
